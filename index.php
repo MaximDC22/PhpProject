da83560917c3b208874f0d1ecff942c2d9e4536c
Nathalie Thillo <nathaliethillo@MacBook-Pro-van-Nathalie.local>
2021-05-03 14:10:43 +0200

    Login

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if(checkLogin($username,$password)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
