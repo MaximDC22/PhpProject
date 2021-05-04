3ae389f51aa0fc1d38384f69e7b5e9321fb14ab8
hippo159 <lukasvd420@gmail.com>
2021-05-04 20:03:17 +0200

    fix:login oop maken

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 <?php
 
-var_dump($_POST);
-$error = false;
-
-function checkLogin($username,$password){
-    if( $username === "manpersoon" && $password === "debouwer" ){
-    return true;
-    }else{
-        return false;
-    }
-}
+$username ="";
+$password = "";
 
 if(!empty($_POST)){
     $username = $_POST['username'];
     $password = $_POST['password'];
-}
 
-if(checkLogin($username,$password)){
-    session_start();
-    $_SESSION["username"]=$username;
-    header("Location: pagina2.php");
     
-} else {
-    $error = true;
+    $conn = new PDO('mysql:host=localhost;dbname=cliptok', 'root', 'root');
+    $stmnt = $conn->prepare("select password from user where username = ':username'");
+    $stmnt->bindValue(":username", $username);
+    $stmnt->execute();
+    $result = $stmnt->fetchAll();
+    return $result;
+    
+
+
 }
-var_dump(checkLogin($username,$password));
+var_dump($result);
+
+
+//if(checkLogin($verify)){
+  //  session_start();
+    // $_SESSION["username"]=$username;
+    //header("Location: pagina2.php");
+    
+//} else {
+//    $error = true;
+//}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -37,23 +43,16 @@ var_dump(checkLogin($username,$password));
 </head>
 <body>
     <h1>test login 1</h1>
-    <?php if($error == true): ?>
-
-        
-        <h3>Login is wrong </h3>
-        
-
-    <?php endif; ?>
-    <form action="#" method ="post" id="loginform">
+    
+    <form action="" method ="POST" id="loginform">
     <label for="username"> Username </label>
     <input type="text" id="username" name="username">
     <label for="password"> Password </label>
     <input type="password" name="password" id="password">
     <input type="submit" value="log in">
     </form>
-        <?php if(isset($_SESSION["username"])):?>
-            <h3>welcome to the website you rapscallion!</h3>
-            <a href="logout.php">click here to log out matey</a>
-        <?php endif?>
+    <a href="http://localhost/login/register.php">register</a>
+        
+    
 </body>
 </html>
